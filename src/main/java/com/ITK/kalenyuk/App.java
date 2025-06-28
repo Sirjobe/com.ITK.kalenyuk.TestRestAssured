f36e1ed9384a590dca82e70dce4f26b78e775ef1
Sirjobe <kalenyuk1998@gmail.com>
2025-06-28 23:09:21 +0300

    ...

---
+++ src/main/java/com/ITK/kalenyuk/App.java
@@ -0,0 +1,13 @@
+package com.ITK.kalenyuk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
